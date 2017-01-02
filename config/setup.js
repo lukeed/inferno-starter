b7c95d6c45b70ae4b3541cea44b004bea7267cef
Luke Edwards <luke.edwards05@gmail.com>
2017-01-02 11:25:08 -0800

    move webpack env & plugin handling to 'setup'

---
+++ config/setup.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const V8LazyParse = require('v8-lazy-parse-webpack-plugin');
+const SWPrecache = require('sw-precache-webpack-plugin');
+const HTML = require('html-webpack-plugin');
+
+const uglify = require('./uglify');
+
+const env = process.env.NODE_ENV || 'development';
+const isProd = (env === 'production');
+
+// base plugins array
+const plugins = [
+	new V8LazyParse(),
+	new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(env)}),
+	new HTML({template: 'src/index.html'})
+];
+
+if (isProd) {
+	plugins.push(
+		new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
+		new webpack.optimize.UglifyJsPlugin(uglify),
+		new SWPrecache({
+			cacheId: 'inferno-starter',
+			filename: 'service-worker.js',
+			dontCacheBustUrlsMatching: /./,
+			staticFileGlobsIgnorePatterns: [/\.(html|map)$/]
+		})
+	);
+} else {
+	// dev only
+	plugins.push(
+		new webpack.HotModuleReplacementPlugin()
+	);
+}
+
+exports.env = env;
+exports.isProd = isProd;
+exports.plugins = plugins;
--- config/webpack.js
+++ config/webpack.js
@@ -1,42 +1,12 @@
 const { join } = require('path');
-const webpack = require('webpack');
-const V8LazyParse = require('v8-lazy-parse-webpack-plugin');
-const SWPrecache = require('sw-precache-webpack-plugin');
-const HTML = require('html-webpack-plugin');
-
-const uglify = require('./uglify');
+const { isProd, plugins } = require('./setup');
 const babel = require('./babel');
 
 const out = join(__dirname, '../dist');
 const exclude = /(node_modules|bower_components)/;
 
-const env = process.env.NODE_ENV || 'development';
-const isProd = env === 'production';
-
-// base plugins array
-const plugins = [
-	new V8LazyParse(),
-	new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(env)}),
-	new HTML({template: 'src/index.html'})
-];
-
 if (isProd) {
 	babel.presets.push('babili');
-	plugins.push(
-		new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
-		new webpack.optimize.UglifyJsPlugin(uglify),
-		new SWPrecache({
-			cacheId: 'inferno-starter',
-			filename: 'service-worker.js',
-			dontCacheBustUrlsMatching: /./,
-			staticFileGlobsIgnorePatterns: [/\.(html|map)$/]
-		})
-	);
-} else {
-	// dev only
-	plugins.push(
-		new webpack.HotModuleReplacementPlugin()
-	);
 }
 
 module.exports = {
