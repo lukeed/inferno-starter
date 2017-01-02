4cdd821930ceb42724aab0891b18e2494df2268f
Luke Edwards <luke.edwards05@gmail.com>
2017-01-02 10:46:21 -0800

    attach SW (prod) if supported && https

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import App from './tags/app';
 import './index.sass';
 
 Inferno.render(<App />, document.getElementById('app'));
+
+// cache all assets if browser supports serviceworker
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator && location.protocol === 'https:') {
+	navigator.serviceWorker.register('/service-worker.js');
+}
