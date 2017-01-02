7fc6c61dcc471c362c9f17a55a6b5fdb9b2bbb70
Luke Edwards <luke.edwards05@gmail.com>
2017-01-02 13:15:22 -0800

    clean dist before builds

--- config/setup.js
+++ config/setup.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const V8LazyParse = require('v8-lazy-parse-webpack-plugin');
 const ExtractText = require('extract-text-webpack-plugin');
 const SWPrecache = require('sw-precache-webpack-plugin');
+const Clean = require('clean-webpack-plugin');
 const HTML = require('html-webpack-plugin');
 
 const uglify = require('./uglify');
@@ -11,6 +12,7 @@ const isProd = (env === 'production');
 
 // base plugins array
 const plugins = [
+	new Clean(['dist']),
 	new V8LazyParse(),
 	new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(env)}),
 	new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}),
@@ -20,12 +22,13 @@ const plugins = [
 if (isProd) {
 	plugins.push(
 		new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
-		new webpack.optimize.UglifyJsPlugin(uglify),
 		new ExtractText('styles.[hash].css'),
+		new webpack.optimize.UglifyJsPlugin(uglify),
 		new SWPrecache({
 			cacheId: 'inferno-starter',
 			filename: 'service-worker.js',
 			dontCacheBustUrlsMatching: /./,
+			navigateFallback: 'index.html',
 			staticFileGlobsIgnorePatterns: [/\.(html|map)$/]
 		})
 	);
