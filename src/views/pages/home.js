3905d4044c07f3393baf087f2d9b3ca796f069af
Luke Edwards <luke.edwards05@gmail.com>
2017-01-04 17:04:33 -0800

    add dashboard card to homepage

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -28,6 +28,20 @@ export default function (props) {
 					<li>Hot Module Replacement (HMR) for all files</li>
 				</ul>
 			</Card>
+
+			<Card>
+				<h2>Dev Dashboard</h2>
+				<p>With <a href="https://github.com/FormidableLabs/webpack-dashboard" target="_blank" rel="noopener">`webpack-dashboard`</a>, it's much easier to see what's happening inside your bundles. In addition to de-cluttering your `webpack-dev-server` log, you can quickly make sense of your bundles' `import`s and sizes.</p>
+
+				<img src="/img/dev-dash.jpg" />
+
+				<p>The dashboard is meant to be interactive (scrollable). If you are having issues, please see the author's note:</p>
+				<p>
+					<blockquote>
+						<strong>OSX Terminal.app users:</strong> Make sure that <strong>View → Allow Mouse Reporting</strong> is enabled, otherwise scrolling through logs and modules won't work. If your version of Terminal.app doesn't have this feature, you may want to check out an alternative such as <a href="https://www.iterm2.com/index.html" target="_blank" rel="noopener">iTerm2</a>.
+					</blockquote>
+				</p>
+			</Card>
 		</div>
 	);
 }
