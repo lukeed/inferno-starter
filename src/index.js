4afbd3968dfcafdd858bbc6fdb13450288ee331e
Luke Edwards <luke.edwards05@gmail.com>
2017-01-04 17:48:16 -0800

    insert ga tracking id

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	ga('create', 'UA-XXXXXXXX-X', 'auto');
+	ga('create', 'UA-71341501-2', 'auto');
 	ga('send', 'pageview');
 
 	// track pages on route change
